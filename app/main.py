795a9f7772fd2ae6c056084ab2c48b6d2f52afdf
Ahmed Saad <ahmeds.elghareeb@gmail.com>
2020-10-19 00:11:59 +0200

    Ops: add lambda needed configuration

--- app/main.py
+++ app/main.py
@@ -1,25 +1,33 @@
 import os
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+
 from app.api.enrolments import router as v1_router
 
+API_GATEWAY_STAGE_PREFIX = os.environ.get("STAGE_PREFIX", default="")
+API_GATEWAY_SERVICE_PREFIX = os.environ.get("SERVICE_PREFIX", default="")
+
 app = FastAPI(
-    title='Enrolments API',
-    openapi_prefix=os.environ.get('STAGE_PREFIX') or ''
+    title="Enrolments API",
+    root_path=API_GATEWAY_STAGE_PREFIX,
+    openapi_url=API_GATEWAY_SERVICE_PREFIX + "/openapi.json",
+    docs_url=API_GATEWAY_SERVICE_PREFIX + "/docs",
 )
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins='*',
+    allow_origins="*",
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(v1_router)
+app.include_router(v1_router, prefix=API_GATEWAY_SERVICE_PREFIX)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # TODO: could be moved into a separate docs-specific entrypoint as
     # this is only called to run docs
     import uvicorn
+
     uvicorn.run(app)
